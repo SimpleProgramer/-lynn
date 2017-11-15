8aa639d8fb978c1c4f0fcf2f5e683a48764a641d
SimpleProgramer <1125334796@qq.com>
2017-11-15 10:00:06 +0800

    集成generator mybatis多数据源读写分离1写2读

---
+++ src/main/java/com/lynn/net/commons/util/mybatis/DataSourceConfiguration.java
@@ -0,0 +1,45 @@
+package com.lynn.net.commons.util.mybatis;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * 加载数据库配置,1主2从
+ * @author wz
+ * */
+@Configuration
+public class DataSourceConfiguration {
+	@Value("${datasource.type}")
+	private Class<? extends DataSource> dataSourceType;
+	
+	@Bean(name="writeDataSource")
+	@Primary
+	@ConfigurationProperties(prefix="datasource.write")
+	public DataSource writeDataSource() {
+		return DataSourceBuilder.create().type(dataSourceType).build();
+	}
+	
+	 /**
+     * 有多少个从库就要配置多少个
+     * @return
+     */
+    @Bean(name = "readDataSource1")
+    @ConfigurationProperties(prefix = "datasource.read1")
+    public DataSource readDataSourceOne() {
+        return DataSourceBuilder.create().type(dataSourceType).build();
+    }
+
+    @Bean(name = "readDataSource2")
+    @ConfigurationProperties(prefix = "datasource.read2")
+    public DataSource readDataSourceTwo() {
+        return DataSourceBuilder.create().type(dataSourceType).build();
+    }
+}
